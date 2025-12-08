2ed880ec2a85e5c20a1a32ebfbfa0d1fe827eb41
chengfangfang <chengfangfang@baidu.com>
2025-12-08 11:22:20 +0800

    first commit 对话式agent开发

---
+++ my-app/src/components/entry/index.jsx
@@ -0,0 +1,17 @@
+/**
+ * @file 入口文件
+ */
+
+import Chat from '../chat';
+import Lazyload from '../lazyload';
+import './style.less'
+
+const Entry = () => {
+    return (
+        <div>
+            <Chat />
+        </div>
+    );
+};
+
+export default Entry;
